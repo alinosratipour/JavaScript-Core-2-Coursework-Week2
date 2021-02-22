4724b47ff90d0a5231a62142d45f9fc205a7dcfb
alinosratipour <alinosratipour@gmail.com>
2021-02-22 01:28:51 +0000

    A-list-of-names- completed

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,20 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+
+    arrayOfPeople.forEach(People =>{
+      //first we need to create our element and assigned it to variable.
+      let header1 = document.createElement("h1");
+      let header2 = document.createElement("h2");
+
+      //here we assign our values to our elements to be shown on the page.
+      header1.innerText = People.name;
+      header2.innerText = People.job;
+
+      //we then append our headers to our content div
+      content.appendChild(header1);
+      content.appendChild(header2);
+    })
+
 }
 
 let people = [
@@ -7,5 +22,8 @@ let people = [
   { name: "Joanna", job: "Student" },
   { name: "Boris", job: "Prime Minister" },
 ];
+
+
+
 
 listOfNames(people);
