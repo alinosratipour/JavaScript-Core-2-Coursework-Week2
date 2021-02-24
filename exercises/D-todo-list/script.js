9fbe717d388430bf0eee1aa61ce6d4a6f91c0f61
alinosratipour <alinosratipour@gmail.com>
2021-02-24 03:54:28 +0000

    task D-toDo-list completed

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,26 @@
 function todoList(todos) {
-  // Write your code here...
+    let content = document.querySelector('#content');
+    let ul   = document.createElement('ul');
+
+ todos.forEach( bj =>{
+    let li = document.createElement('li');
+
+    //toggles line-through when clicked
+function toggleToDo(){
+  if(li.style.textDecoration == "line-through"){
+          li.style.textDecoration = "none";
+        }else{
+          li.style.textDecoration = "line-through"
+      }
+}
+    //assigned todos to elements
+    li.innerText = `${bj.todo}`;
+    // add event listener to call 
+    li.addEventListener("click" , toggleToDo);
+    ul.appendChild(li);
+});
+
+content.appendChild(ul);
 }
 
 const todos = [
