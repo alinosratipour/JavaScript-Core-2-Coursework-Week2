43c5b30a4e9ee54957540d4abdef159d6284ab82
alinosratipour <alinosratipour@gmail.com>
2021-02-24 17:02:32 +0000

    completed tasks A-E

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,33 @@
 function listOfColours(colours) {
-  // Write your code here...
+ let content = document.querySelector("#content");
+ let p = document.createElement("p");
+ let menu = document.createElement("select");
+
+colours.forEach(color => {
+let menuItem = document.createElement("option");
+
+// function to get which color was chosen
+function getColor(){
+    if(menuItem.value == color){
+        p.style.background = this.value;
+        p.style.width = "30%";
+        p.style.padding = "30px";
+        p.innerText = `You selected a: ${menu.value}  `;
+    }
+}
+
+menu.appendChild(menuItem);
+menu.addEventListener('change', getColor);
+
+// get menu items and their values
+menuItem.innerText =color;
+menuItem.value = color;
+
+
+});
+
+content.appendChild(menu);
+content.appendChild(p);
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
